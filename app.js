bb06c8273005251b715ef8ab1a22eb8d57513524
hideyasu2015 <hideyasu.tokuhara@gmail.com>
2020-12-27 22:25:36 +0700

    change port

--- app.js
+++ app.js
@@ -861,6 +861,10 @@ function isDefined(obj) {
 }
 
 // Spin up the server
-app.listen(app.get('port'), function () {
-    console.log('running on port', app.get('port'))
-})
+// app.listen(app.get('port'), function () {
+//     console.log('running on port', app.get('port'))
+// })
+
+app.listen(process.env.PORT, || 3000 function () {
+    console.log('running on port %d in %s', this.address().port, app.settings.env)
+});
